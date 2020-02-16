66c1dc14556c0b8ef87260afe4118edd920a717f
Grzegorz Pietrzak <kotakt@gpietrzak.de>
2020-02-16 21:52:00 +0100

    Initialized board and pool component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PoolComponent } from './pool/pool.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PoolComponent
+    PoolComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
